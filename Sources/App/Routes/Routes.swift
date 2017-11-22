a43ccb1aac6de0b2f126686aa56db399f8dd47c6
hirakida <hirakida29@gmail.com>
2017-11-22 15:47:10 +0900

    use leaf-provider

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -2,6 +2,10 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
+        get { req in
+            return try self.view.make("index")
+        }
+
         get("hello") { req in
             var json = JSON()
             try json.set("hello", "world")
